63a53f864a57607849b7e1f0c1a5d1d60fd89eb7
Mayara Martins <git.qodeless@outlook.com>
2021-12-03 17:36:37 -0300

    view model

---
+++ qodeless.services.WebAPI/Model/MessageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace qodeless.services.WebApi.Model
+{
+    public class MessageViewModel
+    {
+        public string Name { get; set; }
+       
+    }
+}
